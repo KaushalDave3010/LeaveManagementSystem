3dbc6ac5d343d11f84714453df17712f74923a48
Kaushal Dave <kndave3010@gmail.com>
2020-06-25 10:34:59 +0530

    Add project files.

---
+++ LeaveManagementSystem/Data/Employee.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagementSystem.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string TaxId { get; set; }
+
+        public DateTime DataOfBirth { get; set; }
+
+        public DateTime DataOfJoined { get; set; }
+
+    }
+}
---
+++ LeaveManagementSystem/Mappings/Maps.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using LeaveManagementSystem.Data;
+using LeaveManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagementSystem.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<LeaveType, DetailsLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveType, LeaveHistoryVM>().ReverseMap();
+            CreateMap<LeaveType, LeaveAllocationVM>().ReverseMap();
+            CreateMap<LeaveType, EmployeeVM>().ReverseMap();
+        }
+    }
+}
